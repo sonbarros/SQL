4b4ffad55d7a938afeaea02f1562f18b3e300bbb
Anderson S Barros <anderson21br@gmail.com>
2018-04-09 16:51:30 -0300

    any - all primeiro commit

---
+++ any-all.sql
@@ -0,0 +1,13 @@
+/* 
+  The SQL ANY and ALL Operators
+  * The ANY and ALL operators are used with a WHERE or HAVING clause.
+  * The ANY operator returns true if any of the subquery values meet the condition. <---
+  * The ALL operator returns true if all of the subquery values meet the condition. <---
+*/
+
+SELECT ProductName
+FROM Products
+WHERE ProductID = ANY (SELECT ProductID FROM OrderDetails WHERE Quantity = 10);
+
+
+
